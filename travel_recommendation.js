a39d9494d34f2493b056f823aeceb6cf23579193
losamn333 <losman333@hotmail.com>
2026-05-26 09:29:48 -0400

    news

---
+++ travel_recommendation.js
@@ -0,0 +1,18 @@
+function searchCountries() {
+    const input = getElementById('countries')
+    const resultDiv = 
+    resultDiv.innerHTML
+
+    fetch()
+        .then()
+        .then({
+            if () {
+                
+            } else {
+
+            }
+        })
+        .catch( {
+            
+        })
+}
