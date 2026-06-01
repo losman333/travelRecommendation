671b1176d777f59694d15fdeb2f5646dc6da0362
losman333 <losman333@hotmail.com>
2026-06-01 17:14:05 -0400

    adfad

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -3,22 +3,28 @@ function searchCondition() {
 		const resultDiv = document.getElementById('result');
 		resultDiv.innerHTML = '';
 
-		fetch('health_analysis.json')
+		fetch('travel_recommendation_api.json')
 		  .then(response => response.json())
 		  .then(data => {
 			const condition = data.conditions.find(item => item.name.toLowerCase() === input);
 
-			if (condition) {
-			  const symptoms = condition.symptoms.join(', ');
-			  const prevention = condition.prevention.join(', ');
-			  const treatment = condition.treatment;
+			if (destination && keyword) {
+			  const countries = country.countries.join(', ');
+			  const temples = condition.temples.join(', ');
+			  const beaches = condition.beaches;
 
-			  resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
-			  resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
+			  resultDiv.innerHTML += `<h2>${countries.name}</h2>`;
+			  resultDiv.innerHTML += `<img src="${countries.imagesrc}" alt="hjh">`;
 
-			  resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
-			  resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
-			  resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;
+              resultDiv.innerHTML += `<h2>${beaches.name}</h2>`;
+			  resultDiv.innerHTML += `<img src="${beaches.imageUrl}" alt="hjh">`;
+
+              resultDiv.innerHTML += `<h2>${temples.name}</h2>`;
+			  resultDiv.innerHTML += `<img src="${temples.imagesrc}" alt="hjh">`;
+
+			  resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${countries}</p>`;
+			  resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${beaches}</p>`;
+			  resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${temples}</p>`;
 			} else {
 			  resultDiv.innerHTML = 'Condition not found.';
 			}
