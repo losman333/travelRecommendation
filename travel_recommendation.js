b65f9e1ae8545d68da20a02e86d41070b8ce09cc
losman333 <losman333@hotmail.com>
2026-06-02 09:26:22 -0400

    adf

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,4 +1,4 @@
-function searchCondition() {
+function searchDestination() {
 		const input = document.getElementById('conditionInput').value.toLowerCase();
 		const resultDiv = document.getElementById('result');
 		resultDiv.innerHTML = '';
