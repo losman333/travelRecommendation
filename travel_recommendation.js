1affb315310c5f60288b8231ae95cd721e2e66b5
losman333 <losman333@hotmail.com>
2026-06-03 12:33:49 -0400

    adf

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,37 +1,7 @@
-function searchDestination() {
-		const input = document.getElementById('destinationInput').value.toLowerCase();
-		const resultDiv = document.getElementById('result');
-		resultDiv.innerHTML = '';
+const btnSearch = document.getElementById("btnSearch");
 
-		fetch('travel_recommendation_api.json')
-		  .then(response => response.json())
-		  .then(data => {
-			const destination = data.countries.find(item => item.name.toLowerCase() === input);
+function searchDestination () {
+    const destinationInput = document.getElementById("destinationInput");
+    
 
-			if (destination) {
-			  const countries = destination.countries.join(', ');
-			  const temples = destination.temples.join(', ');
-			  const beaches = destination.beaches.join(', ');
-
-			  resultDiv.innerHTML += `<h2>${countries.name}</h2>`;
-			  resultDiv.innerHTML += `<img src="${countries.imagesrc}" alt="hjh">`;
-
-              resultDiv.innerHTML += `<h2>${beaches.name}</h2>`;
-			  resultDiv.innerHTML += `<img src="${beaches.imageUrl}" alt="hjh">`;
-
-              resultDiv.innerHTML += `<h2>${temples.name}</h2>`;
-			  resultDiv.innerHTML += `<img src="${temples.imagesrc}" alt="hjh">`;
-
-			  resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${countries}</p>`;
-			  resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${beaches}</p>`;
-			  resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${temples}</p>`;
-			} else {
-			  resultDiv.innerHTML = 'Condition not found.';
-			}
-		  })
-		  .catch(error => {
-			console.error('Error:', error);
-			resultDiv.innerHTML = 'An error occurred while fetching data.';
-		  });
-	  }
-	    btnSearch.addEventListener('click', searchDestination);
+}
