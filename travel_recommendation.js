c8a921c32560fe054cf4f0b3461b36b0eba56257
losman333 <losman333@hotmail.com>
2026-06-09 01:02:08 -0400

    adf

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,10 +1,29 @@
 btnSearch
 
+/**
+ * recommendattion results
+ */
 searchInput
+
 
 fetch travel_recommendation_api.json
 
+.then
+
+.then
+
+catch
+
+
+
 console.log(result)
+
+
+/**
+ * keyword searches
+ */
+
+
 
 beach beaches Beach
 
@@ -12,8 +31,19 @@ temple temples Temple
 
 country countries Country
 
+/**
+ * task recommendations
+ */
+
+
 Image
 
 description
 
 result
+
+/**
+ * clear button
+ */
+
+clearBtn
