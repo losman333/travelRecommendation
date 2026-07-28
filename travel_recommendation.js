85cad6becc8018bbd062f65d99f367626d41fe95
losman333 <losman333@hotmail.com>
2026-07-28 01:23:04 -0400

    adf

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -3,7 +3,7 @@ let result = document.getElementById('result');
 fetch ('travel_recommendation_api.json')
 .then(response => response.json())
 .then(data => {
-   const data = data.countries  
+   const data = data.countries.find(item => item.name.toLowerCase) === input);  
 
    /**
     * console.log(Country Name:`${countries.name}`)
