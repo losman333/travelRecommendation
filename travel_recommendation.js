852ced8e00ec1c7f60853df6dbfc2a25fc0a4888
losman333 <lsoman333@hotmail.com>
2026-07-30 13:01:05 -0400

    adf

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -3,10 +3,26 @@ let result = document.getElementById('result');
 fetch ('travel_recommendation_api.json')
 .then(response => response.json())
 .then(data => {
-   const data = data.countries.find(item => item.name.toLowerCase) === input);  
+   data.countries.forEach(country => {
+        country.cities.forEach(city => {
+        result.innerHTML += `
+        <div class = "container"> 
+            <div class="card">
+                <h3>${city.imageURL}</h3>
+                <h2>${city.name}</h2>
+                <img src="${city.description}" alt="${city.name}" width="300">
+            </div>
+        </div>    
+        `
+        ;
+        });
 
-   /**
-    * console.log(Country Name:`${countries.name}`)
+   });
+        
+    
+// {country.name}
+
+    // console.log(Country Name:`${countries.name}`)
  
-
 })
+// 
