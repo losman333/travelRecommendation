9b523cfaa670b6c0a915e8ed1f898d682c7c0aa5
losman333 <losman333@hotmail.com>
2026-07-30 20:42:02 -0400

    adsf

---
+++ searchPlaces.js
@@ -0,0 +1,20 @@
+// 
+function searchPlaces() {
+    const input = document.getElementById("searchInput")
+        .value
+        .toLowerCase();
+
+    let resultDiv = document.getElementById('result');
+    const search = document.getElementById('searchButton');
+    fetch('travel_recommendation_api.json')
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(country => {
+                country.cities.forEach(cities => {
+                    resultDiv.innerHTML += `${cities.name}`;
+                });
+            });
+        });
+
+
+}
--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,28 +1,28 @@
-
-let result = document.getElementById('result');
-fetch ('travel_recommendation_api.json')
-.then(response => response.json())
-.then(data => {
-   data.countries.forEach(country => {
-        country.cities.forEach(city => {
-        result.innerHTML += `
-        <div class = "container"> 
-        <div class="card">
-            <img src="${city.imageUrl}" alt="${city.imageUrl}" width="300">
-            <h2>${city.name}</h2>
-            <h3>${city.description}</h3>
-        </div>
-        </div>    
-        `
-        ;
-        });
-
-   });
-        
+let searchButton = document.getElementById("searchButton");
     
-// {country.name}
 
-    // console.log(Country Name:`${countries.name}`)
- 
-})
-// 
+function searchPlaces() {
+
+    let resultDiv = document.getElementById('result');
+    const input = document.getElementById("searchInput").value.toLowerCase();
+    fetch ('travel_recommendation_api.json')
+    .then(response => response.json())
+    .then(data => { 
+        const country = data.countries.find(c => c.name.toLowerCase() === input);
+
+    if(country) {
+      
+            country.cities.forEach(cities => {
+                resultDiv.innerHTML += `${cities.imageUrl}`;
+                resultDiv.innerHTML += `${cities.name}`;
+                resultDiv.innerHTML += `${cities.name}`;
+            });
+   
+    };
+
+    });
+    
+     
+}
+
+   searchButton.addEventListener('click', searchPlaces);
