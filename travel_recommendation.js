b718e793880579caf419e3811fb8043bf7763bd8
losman333 <losman333@hotmail.com>
2026-08-09 13:43:03 -0400

    asdfd

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,35 +1,11 @@
+const searchButton = document.getElementById('searchInput');
+const results = document.getElementById('search-results');
 
-document.getElementById("searchInput");
-document.getElementById("searchButton");
-document.getElementById("search-results");
-"result"
-resultDiv.innerHTML = '';
-
-
-fetch("trave_recommendation_api.json")
-.then(response => response.json());
-.then(data => {
-    destinationInput
-    result
-
-    if(destinations) {
-        // check to see if search matches the entered input 
-         const destination = data.countries.find(item => item.name.toLowerCase.include() === input)
-
-            resultDiv.innerHTML =+ `
-            
-            ${destination.name}
-            ${destination.description}
-            
-            `
-         
-    } else {
-
-    }
-
-
-}) catch(error => {
-    "no places found"
-})
-
-searchButton.addEventListener();
+function searchDestinations() {
+    fetch('travel_recommendation_api.json')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+}
+searchButton.addEventListener('click', searchDestinations);
