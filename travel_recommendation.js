444e39f3c71cc1b16d3843bed0c778177435d333
losman333 <losman333@hotmail.com>
2026-08-14 01:15:51 -0400

    adf

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -9,10 +9,14 @@ function searchDestinations() {
     fetch('travel_recommendation_api.json')
     .then(response => response.json())
     .then(data => {
-       if( input === 'beaches' || input === 'beaches') {
-           data.beaches.forEach(beach => {
-                resultDiv.innerHTML += `${beach.name} ${beach.description}`
+       if(input === 'beaches' || input === 'BEACHES') {
+           data.beaches.forEach(p => {
+                resultDiv.innerHTML += `${p.name} ${p.description}`
            })
+       } else if (input === 'temples' | 'TEMPLES') {
+            data.temples.forEach(t => {
+                resultDiv.innerHTML += `${t.name} ${t.description}`
+            })
        }
 
     });
