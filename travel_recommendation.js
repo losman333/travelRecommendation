f917dc8bd63c6446132ca1fc5f8f9755998cbf3b
losman333 <losman333@hotmail.com>
2026-08-14 13:36:16 -0400

    adf

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -9,15 +9,35 @@ function searchDestinations() {
     fetch('travel_recommendation_api.json')
     .then(response => response.json())
     .then(data => {
-       if(input === 'beaches' || input === 'BEACHES') {
-           data.beaches.forEach(p => {
-                resultDiv.innerHTML += `${p.name} ${p.description}`
-           })
-       } else if (input === 'temples' | 'TEMPLES') {
-            data.temples.forEach(t => {
-                resultDiv.innerHTML += `${t.name} ${t.description}`
+        if(data[input]) {
+            data[input].forEach(p => {
+                if(input === "countries") {
+                    p.cities.forEach(city => {
+                        resultDiv.innerHTML += `
+                        <h3>${city.name}</h3>
+                        <img src="${city.imageUrl}" alt="${city.name} "width="400" >
+                        <p>${city.description}</p>
+                        `;
+                    })
+                } else {
+                    resultDiv.innerHTML += `
+                        <h3>${p.name}</h3>
+                        <img src="${p.imageUrl}" alt="${p.description}" width="400px">
+                        <p>${p.description}</P>
+                        `;
+                }
             })
-       }
+        } 
+       
+    //    if(input === 'beaches' || input === 'BEACHES') {
+    //        data.beaches.forEach(p => {
+    //             resultDiv.innerHTML += `${p.name} ${p.description}`
+    //        })
+    //    } else if (input === 'temples' | 'TEMPLES') {
+    //         data.temples.forEach(t => {
+    //             resultDiv.innerHTML += `${t.name} ${t.description}`
+    //         })
+    //    }
 
     });
 }
